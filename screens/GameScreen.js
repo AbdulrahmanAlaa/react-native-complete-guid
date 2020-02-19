ad14bd7cf08baaf83530b1680fca445151f64368
Abdulrahman Alaa Eldin  <abdulrahmanalaa@hotmail.com>
2020-02-19 13:29:58 +0400

    Adding Game Screen with guess function

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import {View,Text,StyleSheet} from 'react-native';
+import React,{useState} from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-const GameScreen = (props)=> {
-
+const generateRamdomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndNumber = Math.floor(Math.random() * (max - min) - min);
+  if (rndNumber === exclude) {
+    generateRamdomBetween(min, max, exclude);
+  } else {
+    return rndNumber;
+  }
 };
+
+const GameScreen = ({userChoice}) => {
+    const [currentGuess, setCurrentGuess] = useState(generateRamdomBetween(1,100,userChoice))
+};
+
 const styles = StyleSheet.create({});
+
 export default GameScreen;
